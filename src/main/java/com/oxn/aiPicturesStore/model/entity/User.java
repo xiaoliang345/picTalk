f999a343cc89cd7c0cc21a9741cb6ab9756e768a
赤崽 <3457674636@qq.com>
2025-10-10 15:07:40 +0800

    用户登录信息存入redis

--- src/main/java/com/oxn/aiPicturesStore/model/entity/User.java
+++ src/main/java/com/oxn/aiPicturesStore/model/entity/User.java
@@ -2,6 +2,7 @@ package com.oxn.aiPicturesStore.model.entity;
 
 import com.baomidou.mybatisplus.annotation.*;
 
+import java.io.Serializable;
 import java.util.Date;
 
 import io.swagger.annotations.ApiParam;
@@ -13,7 +14,7 @@ import lombok.Data;
  */
 @TableName(value ="user")
 @Data
-public class User {
+public class User implements Serializable {
     /**
      * id
      */
